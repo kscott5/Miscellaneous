e9f2369e573f88fa60d1945b0da20a2bcc37cf43
Karega K. Scott <kscott@pcsc.co>
2016-03-01 20:49:02 -0600

    Initial commit

---
+++ test/OrgiCStar.test.js
@@ -0,0 +1,87 @@
+'use strict';
+
+const assert = require('assert');
+const orgicstar = require('../lib/OrgiCStar');
+
+const USER_INPUT = 'AB5, BC4, CD8, DC8, DE6, AD5, CE2, EB3, AE7';
+const SUCCESS_MSG = 'Completed Successfully!';
+const FAILURE_MSG = 'NO SUCH ROUTE';
+	
+describe('OrgiCStar test suite', function() {
+	it('The distance of the route A-B-C is 9', function() {
+		let org = new orgicstar();
+		org.initializeGraph(USER_INPUT);
+
+		var actual = org.routeCompletePath('A-B-C');
+		
+		assert.ok(actual.valid);
+		assert.equal(actual.message, SUCCESS_MSG);
+		assert.equal(actual.cost, /*expect*/ 9);
+	});
+
+	it('The distance of the route A-D is 5', function() {
+		let org = new orgicstar();
+		org.initializeGraph(USER_INPUT);
+
+		var actual = org.routeCompletePath('A-D');
+		
+		assert.ok(actual.valid);
+		assert.equal(actual.message, SUCCESS_MSG);
+		assert.equal(actual.cost, /*expect*/ 5);	
+	});
+
+	it('The distance of the route A-D-C is 13', function() {
+  		let org = new orgicstar();
+		org.initializeGraph(USER_INPUT);
+
+		var actual = org.routeCompletePath('A-D-C');
+		
+		assert.ok(actual.valid);
+		assert.equal(actual.message, SUCCESS_MSG);
+		assert.equal(actual.cost, /*expect*/ 13);
+	});
+
+	it('The distance of the route A-E-B-C-D is 22', function() {
+  		let org = new orgicstar();
+		org.initializeGraph(USER_INPUT);
+
+		var actual = org.routeCompletePath('A-E-B-C-D');
+		
+		assert.ok(actual.valid);
+		assert.equal(actual.message, SUCCESS_MSG);
+		assert.equal(actual.cost, /*expect*/ 22);
+	});
+
+	it('The distance of the route A-E-D is \'NO SUCH ROUTE\'', function() {
+	  let org = new orgicstar();
+		org.initializeGraph(USER_INPUT);
+
+		var actual = org.routeCompletePath('A-E-D');
+		
+		assert.ok(!actual.valid);
+		assert.equal(actual.message, FAILURE_MSG);
+		assert.equal(actual.cost, /*expect*/ 0);		
+	});
+	
+	it('The number of trips starting at C and ending at C with a maximum of 3. Total paths 2', function() {
+	  let org = new orgicstar();
+		org.initializeGraph(USER_INPUT);
+
+		var actual = org.routeAllPathsThatStartEndWith('C', 'C', function(nodesInPath){return nodesInPath<=3});
+		
+		assert.ok(actual.valid);
+		assert.equal(actual.message, SUCCESS_MSG);
+		assert.equal(actual.cost, /*expect*/ 2);		
+	});
+	
+	it('The number trips starting at A and ending at C with a exactly 4 stops. Total paths 3', function() {
+	  let org = new orgicstar();
+		org.initializeGraph(USER_INPUT);
+
+		var actual = org.routeAllPathsThatStartEndWith('A', 'C', function(nodesInPath){return nodesInPath==4});
+		
+		assert.ok(actual.valid);
+		assert.equal(actual.message, SUCCESS_MSG);
+		assert.equal(actual.cost, /*expect*/ 3);		
+	});
+});
