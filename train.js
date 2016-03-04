49c76f217360ddde4c412d3def9adc7732dc10a2
Karega K. Scott <kscott@pcsc.co>
2016-03-04 15:15:59 -0600

    Changed method call for OrgiCStar

--- train.js
+++ train.js
@@ -1,20 +1,20 @@
 'use strict';
 
-const OrgiCStar = require('../lib/OrgiCStar')
+const OrgiCStar = require('./lib/OrgiCStar')
 var org = new OrgiCStar();
 
 console.log('AB5, BC4, CD8, DC8, DE6, AD5, CE2, EB3, AE7');
 org.initializeGraph('AB5, BC4, CD8, DC8, DE6, AD5, CE2, EB3, AE7');
 
 
-console.log(org.routeCompletePath('A-B-C'));
-console.log(org.routeCompletePath('A-D'));
-console.log(org.routeCompletePath('A-D-C'));
-console.log(org.routeCompletePath('A-E-B-C-D'));
-console.log(org.routeCompletePath('A-E-D'));
-console.log(org.routeAllPathsThatStartEndWith({startNodeId:'C', endNodeId: 'C', comparer: function(comparerArgs) { return comparerArgs.nodesInPath<=3;}}));
-console.log(org.routeAllPathsThatStartEndWith({startNodeId:'A', endNodeId: 'C', comparer: function(comparerArgs) { return comparerArgs.nodesInPath>=4;}}));
-console.log(org.routeAllPathsThatStartEndWith({startNodeId:'A', endNodeId: 'C', shortestRoute: true}));
-console.log(org.routeAllPathsThatStartEndWith({startNodeId:'B', endNodeId: 'B', shortestRoute: false}));
+console.log(org.routePath('A-B-C'));
+console.log(org.routePath('A-D'));
+console.log(org.routePath('A-D-C'));
+console.log(org.routePath('A-E-B-C-D'));
+console.log(org.routePath('A-E-D'));
+console.log(org.routePath({startNodeId:'C', endNodeId: 'C', comparer: function(comparerArgs) { return comparerArgs.nodesInPath<=3;}}));
+console.log(org.routePath({startNodeId:'A', endNodeId: 'C', comparer: function(comparerArgs) { return comparerArgs.nodesInPath>=4;}}));
+console.log(org.routePath({startNodeId:'A', endNodeId: 'C', shortestRoute: true}));
+console.log(org.routePath({startNodeId:'B', endNodeId: 'B', shortestRoute: false}));
 
 
